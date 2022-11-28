98605758988cd3b5ecd1fb708f21d13af4798ebe
shantelraya <parischax@gmail.com>
2022-11-28 22:51:21 +0300

    created serializers

---
+++ db/migrate/20221128192443_create_restaurant_pizzas.rb
@@ -0,0 +1,10 @@
+class CreateRestaurantPizzas < ActiveRecord::Migration[7.0]
+  def change
+    create_table :restaurant_pizzas do |t|
+      t.integer :restaurant_id
+      t.integer :pizza_id
+
+      t.timestamps
+    end
+  end
+end
